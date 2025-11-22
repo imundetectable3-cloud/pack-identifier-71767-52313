69e7851bffa99954466429cfb2a32ee0f8622004
imundetectable3-cloud <imundetectable3@gmail.com>
2025-11-22 23:39:13 +0530

    Fix NotFound link to use React Router Link component

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";, Link
 import { useEffect } from "react";
 
 const NotFound = () => {
@@ -13,10 +13,8 @@ const NotFound = () => {
       <div className="text-center">
         <h1 className="mb-4 text-4xl font-bold">404</h1>
         <p className="mb-4 text-xl text-gray-600">Oops! Page not found</p>
-        <a href="/" className="text-blue-500 underline hover:text-blue-700">
-          Return to Home
-        </a>
-      </div>
+        <Link to="/" className="text-blue-500 underline hover:text-blue-700">          Return to Home
+        </Link>      </div>
     </div>
   );
 };
